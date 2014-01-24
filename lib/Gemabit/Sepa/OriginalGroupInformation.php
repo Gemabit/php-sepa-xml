3f18a0c5d8fa738fe04063ba9ee94c538e28f325
Ricardo Lopes <doutrust@gmail.com>
2014-01-24 11:40:11 +0000

    Done:  - Original group information equals BdP docs

--- lib/Gemabit/Sepa/OriginalGroupInformation.php
+++ lib/Gemabit/Sepa/OriginalGroupInformation.php
@@ -26,4 +26,177 @@ namespace Gemabit\Sepa;
 class OriginalGroupInformation
 {
 
+    /**
+     * @var string original message information identification ([a-zA-Z0-9]){1,35}
+     */
+    protected $originalMessageIdentification;
+
+    /**
+     * @var string original message information name ([a-zA-Z0-9]){1,35}
+     */
+    protected $originalMessageNameIdentification;
+
+    /**
+     * @var string original number of transactions ([a-zA-Z0-9]){1,15}
+     */
+    protected $originalNumberOfTransactions;
+
+    /**
+     * @var double original control sum ([0-9]){1,15}(\,|\.)?([0-9]){0,2}
+     */
+    protected $originalControlSum;
+
+    /**
+     * @var string status reason information proprietary code,
+     *      indicating original message status ([a-zA-Z0-9]){1,35}
+     */
+    protected $statusReasonInformationProprietary;
+
+    /**
+     * @var string transaction amount for the respective message ([a-zA-Z0-9]){1,15}
+     */
+    protected $detailedNumberOfTransactionsPerStatus;
+
+    /**
+     * @var string common status to all the reported messages ([a-zA-Z0-9]){4}
+     */
+    protected $detailedStatus;
+
+    /**
+     * @var string total amount for the reported transactions ([0-9]){1,15}(\,|\.)?([0-9]){0,2}
+     */
+    protected $detailedControlSum;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @param mixed $detailedControlSum
+     */
+    public function setDetailedControlSum($detailedControlSum)
+    {
+        $this->detailedControlSum = $detailedControlSum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetailedControlSum()
+    {
+        return $this->detailedControlSum;
+    }
+
+    /**
+     * @param mixed $detailedNumberOfTransactionsPerStatus
+     */
+    public function setDetailedNumberOfTransactionsPerStatus($detailedNumberOfTransactionsPerStatus)
+    {
+        $this->detailedNumberOfTransactionsPerStatus = $detailedNumberOfTransactionsPerStatus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetailedNumberOfTransactionsPerStatus()
+    {
+        return $this->detailedNumberOfTransactionsPerStatus;
+    }
+
+    /**
+     * @param mixed $detailedStatus
+     */
+    public function setDetailedStatus($detailedStatus)
+    {
+        $this->detailedStatus = $detailedStatus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetailedStatus()
+    {
+        return $this->detailedStatus;
+    }
+
+    /**
+     * @param mixed $originalControlSum
+     */
+    public function setOriginalControlSum($originalControlSum)
+    {
+        $this->originalControlSum = $originalControlSum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOriginalControlSum()
+    {
+        return $this->originalControlSum;
+    }
+
+    /**
+     * @param mixed $originalMessageIdentification
+     */
+    public function setOriginalMessageIdentification($originalMessageIdentification)
+    {
+        $this->originalMessageIdentification = $originalMessageIdentification;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOriginalMessageIdentification()
+    {
+        return $this->originalMessageIdentification;
+    }
+
+    /**
+     * @param mixed $originalMessageNameIdentification
+     */
+    public function setOriginalMessageNameIdentification($originalMessageNameIdentification)
+    {
+        $this->originalMessageNameIdentification = $originalMessageNameIdentification;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOriginalMessageNameIdentification()
+    {
+        return $this->originalMessageNameIdentification;
+    }
+
+    /**
+     * @param mixed $originalNumberOfTrnasactions
+     */
+    public function setOriginalNumberOfTransactions($originalNumberOfTransactions)
+    {
+        $this->originalNumberOfTransactions = $originalNumberOfTransactions;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOriginalNumberOfTransactions()
+    {
+        return $this->originalNumberOfTransactions;
+    }
+
+    /**
+     * @param mixed $statusReasonInformationProprietary
+     */
+    public function setStatusReasonInformationProprietary($statusReasonInformationProprietary)
+    {
+        $this->statusReasonInformationProprietary = $statusReasonInformationProprietary;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatusReasonInformationProprietary()
+    {
+        return $this->statusReasonInformationProprietary;
+    }
 } 
